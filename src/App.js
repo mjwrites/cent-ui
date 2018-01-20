099fb546c853ace3e4a91c4ff798375b82e700c1
Maria-Jose Ramos <mjramos@Maria-Joses-MacBook-Pro.local>
2018-01-20 01:34:35 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// Components
+import { Main } from './components/Main';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Main extends Component {
+    render() {
+        return (
+            <div className="background-color">
+                <div className="container">
+                <div className="landing">
+                    <h1 className="title">Cent.</h1>
+                </div>
+                </div>
+            </div>
+        )
+    }
+} 
